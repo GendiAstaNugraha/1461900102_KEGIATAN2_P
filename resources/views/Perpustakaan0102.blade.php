458049ada51a2553816d7831a759dfbe9c1c781c
GendiAstaNugraha <astagendi@gmail.com>
2021-05-29 17:15:07 +0700

    first commit

--- resources/views/Perpustakaan0102.blade.php
+++ resources/views/Perpustakaan0102.blade.php
@@ -4,28 +4,42 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Pratikum_2</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>PRATIKUM2</title>
   </head>
   <body>
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Kode_Kategori</th>
-            <th>Nama_kategori</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($perpustakaan as $perpus)
+  <H1>SELECT :</H1>
+  <div style="overflow-x: auto;">
+    <table class="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th>Kode Kategori</th>
+          <th>Nama Kategori</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($kategori as $perpus)
           <tr>
             <td>{{$perpus->kode_kategori}}</td>
             <td>{{$perpus->nama_kategori}}</td>
           </tr>
-          @andforeach
-        </tbody>
-      </table>
-    </div>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
   </body>
 </html>
